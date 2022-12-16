61b7b1971d4ce494f0550c96212e65c21213d1ad
jaewoong <icecrao2@daum.net>
2022-12-16 09:24:17 +0900

    init

---
+++ RunningRecords/RunningRecordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningRecordsApp.swift
+//  RunningRecords
+//
+//  Created by sw on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct RunningRecordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
